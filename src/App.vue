5fefd72acdf4fef304cc6c7f2174aef7a030dc05
zhongjiating26 <zhongjiating26@gmail.com>
2025-12-25 16:35:32 +0800

    创建 Vue3 + Vite 项目（现在要做） 依赖安装成功 可以运行项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="app">
+    <h1>社团面试系统 · 前端</h1>
+    <p>项目初始化成功 🎉</p>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  padding: 40px;
+  font-size: 18px;
+}
+</style>
